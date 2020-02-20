15ad52965e67d9d0ee09f012f903a1d25ff8b9df
sharma121 <saurabh.sharma3@non.keysight.com>
2020-02-19 22:20:48 -0800

    update

--- FinalProblem/GoogleHasCode.java
+++ FinalProblem/GoogleHasCode.java
@@ -8,18 +8,14 @@ class GoogleHashCode {
     static int MAX;
     static List<Integer> Input, Output;
     static int Num;
-    static int score = 0;
 
     public static void main(String args[]) {
         String folderName = "C:\\Users\\Saurabh Sharma\\Documents\\personal\\GoogleHashCode\\PracticeProblem\\";
         String[] fileNames = { "a_example", "b_small", "c_medium", "d_quite_big", "e_also_big" };
         for (int i = 0; i < fileNames.length; i++) {
             getInputFromFile(folderName, fileNames[i]);
-            Output = solve(MAX, Input);
-            Collections.reverse(Output);
-
+            //call your solve method
             writeIntoFile(folderName, fileNames[i]);
-            System.out.println("\nSCORE : " + score);
         }
 
     }
@@ -66,43 +62,5 @@ class GoogleHashCode {
         } catch (Exception e) {
             System.err.println("" + e);
         }
-    }
-
-    static List<Integer> solve(int Max, List<Integer> input) {
-        List<Integer> BestSolution = new ArrayList<Integer>();
-        score = 0;
-        int j, i = input.size() - 1;
-        boolean test1 = true, test2 = true;
-        while (i >= 0 && test1) {
-            List<Integer> TempBestSolution = new ArrayList<Integer>();
-            j = i;
-            int sum = 0;
-            while (j >= 0 && test2) {
-
-                int temp = input.get(j);
-                if (sum + temp < Max) {
-                    sum = sum + temp;
-                    TempBestSolution.add(j);
-                } else if (sum + temp == Max) {
-                    sum = sum + temp;
-                    TempBestSolution.add(j);
-                    test2 = false;
-                    test1 = false;
-                }
-                j--;
-            }
-            if (score < sum) {
-                score = sum;
-                BestSolution = TempBestSolution;
-            }
-            if (BestSolution.size() == input.size()) {
-                test1 = false;
-
-            }
-
-            i--;
-        }
-
-        return BestSolution;
     }
 }
